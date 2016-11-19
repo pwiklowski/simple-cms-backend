5640e945a9d142d3237cc7621e1b5967d6d4f029
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-11-19 15:15:58 +0100

    Use better dispatcher

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var http = require('http');
-var HttpDispatcher = require('httpdispatcher');
 var MongoClient = require('mongodb').MongoClient
-
-var dispatcher     = new HttpDispatcher();
+var dispatch = require('dispatch');
 
 const PORT=8080; 
 
@@ -10,37 +8,56 @@ var config = {
   "resources": [
     { url: "/test1", res: "test1", isWritable: true },
     { url: "/test2", res: "test2" },
-    { url: "/test3", res: "test3" },
+    { url: "/test3", res: "test3", isMultiple: true },
     { url: "/test4", res: "test4" }
   ]
 };
 
-config.resources.forEach((resource) =>{
-  dispatcher.onGet(resource.url, function(req, res) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end(resource.res);
-  });    
+var routing = {
+    '/test': {
+        GET: function (req, res, next) {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(resource.res);
+        },
+        POST: function (req, res, next) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(resource.res);
+        }
+    }
+};
 
-  if (resource.isWritable !== undefined && resource.isWritable === true){
-    dispatcher.onPost(resource.url, function(req, res) {
+config.resources.forEach((resource) =>{
+  var r = {};
+  if (resource.isMultiple !== undefined && resource.isMultiple === true){
+    r.GET = (req, res, id) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(resource.res + " " + id);
+    }
+    if (resource.isWritable !== undefined && resource.isWritable === true){
+      r.POST = (req, res, id) => {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(resource.res + " " + id);
+      }
+    }
+    routing[resource.url+"/:id"] = r;
+  }else{
+    r.GET = (req, res, next) => {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end(resource.res);
-    });
+    }
+    if (resource.isWritable !== undefined && resource.isWritable === true){
+      r.POST = (req, res, next) => {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(resource.res);
+      }
+    }
+    routing[resource.url] = r;
   }
 });
 
 console.log(config);
 
-function handleRequest(request, response){
-    try {
-        console.log(request.url);
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-var server = http.createServer(handleRequest);
-
+var server = http.createServer(dispatch(routing));
 server.listen(PORT, function(){
     console.log("Server started");
 });
