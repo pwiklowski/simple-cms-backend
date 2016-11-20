d7616511d6c36a31643619f175a084273eeeca13
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-11-20 13:27:19 +0100

    Use resource name to make urls

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const PORT=8080;
 
 var config = {
   "resources": [
-    { url: "/test1", res: "test1", isWritable: true },
-    { url: "/test2", res: "test2" },
-    { url: "/test3", res: "test3", isMultiple: true, isWritable: true },
-    { url: "/test4", res: "test4" }
+    { name: "cat", isMultiple: true, isWritable: true },
+    { name: "dog", isMultiple: true, isWritable: true },
+    { name: "shoe", isMultiple: true, isWritable: true },
   ]
 };
 
@@ -27,9 +26,9 @@ var db;
 
 config.resources.forEach((resource) =>{
   var r = {};
+  var collection = db.collection(resource.name);
   if (resource.isMultiple !== undefined && resource.isMultiple === true){
     r.GET = (req, res, id) => {
-        var collection = db.collection(resource.res);
         collection.findOne({_id:ObjectId(id)}).then((item) => {
           if (item !== null){
             res.writeHead(200, {'Content-Type': 'application/json'});
@@ -43,20 +42,16 @@ config.resources.forEach((resource) =>{
     if (resource.isWritable !== undefined && resource.isWritable === true)
     {
       r.DELETE = (req, res, id) => {
-        var collection = db.collection(resource.res);
         collection.deleteOne({_id:ObjectId(id)}).then((item) => {
           res.writeHead(200, {'Content-Type': 'application/json'});
           res.end();
         });
       }
       r.PUT = (req, res, id) => {
-        var collection = db.collection(resource.res);
         var jsonString = "";
-
         req.on('data', data => jsonString += data);
         req.on('end', () => {
           var obj = JSON.parse(jsonString);
-          console.log("POST");
           collection.updateOne({_id:ObjectId(id)}, obj, (err, item) => {
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(obj));
@@ -64,11 +59,10 @@ config.resources.forEach((resource) =>{
         });
       }
     }
-    routing[resource.url+"/:id"] = r;
+    routing["/"+resource.name+"/:id"] = r;
 
     var all = {};
     all.GET = (req, res, id) => {
-        var collection = db.collection(resource.res);
         collection.find({}).toArray((err, items) => {
           res.writeHead(200, {'Content-Type': 'application/json'});
           res.end(JSON.stringify(items));
@@ -77,7 +71,6 @@ config.resources.forEach((resource) =>{
     if (resource.isWritable !== undefined && resource.isWritable === true)
     {
       all.PUT = (req, res, id) => {
-        var collection = db.collection(resource.res);
         var jsonString = "";
 
         req.on('data', data => jsonString += data);
@@ -90,19 +83,19 @@ config.resources.forEach((resource) =>{
         });
       }
     }
-    routing[resource.url] = all;
+    routing["/"+resource.name] = all;
   }else{
-    r.GET = (req, res, next) => {
+    r.GET = (req, res, next) => { //TODO
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(resource.res);
+        res.end(resource.name);
     }
     if (resource.isWritable !== undefined && resource.isWritable === true){
-      r.POST = (req, res, next) => {
+      r.POST = (req, res, next) => { //TODO
           res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end(resource.res);
+          res.end(resource.name);
       }
     }
-    routing[resource.url] = r;
+    routing["/"+resource.name] = r;
   }
 });
 
