1d9914161be1355647bcb2bcc0208a8d6b538b34
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-11-20 17:46:56 +0100

    Init after connecting to server

--- index.js
+++ index.js
@@ -23,91 +23,90 @@ var routing = {
 };
 
 var db;
-
-config.resources.forEach((resource) =>{
-  var r = {};
-  var collection = db.collection(resource.name);
-  if (resource.isMultiple !== undefined && resource.isMultiple === true){
-    r.GET = (req, res, id) => {
-        collection.findOne({_id:ObjectId(id)}).then((item) => {
-          if (item !== null){
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(item));
-          }else{
-            res.writeHead(404, {'Content-Type':'application/json'});
-            res.end();
-          }
-        });
-    }
-    if (resource.isWritable !== undefined && resource.isWritable === true)
-    {
-      r.DELETE = (req, res, id) => {
-        collection.deleteOne({_id:ObjectId(id)}).then((item) => {
-          res.writeHead(200, {'Content-Type': 'application/json'});
-          res.end();
-        });
-      }
-      r.PUT = (req, res, id) => {
-        var jsonString = "";
-        req.on('data', data => jsonString += data);
-        req.on('end', () => {
-          var obj = JSON.parse(jsonString);
-          collection.updateOne({_id:ObjectId(id)}, obj, (err, item) => {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(obj));
+var initHandlers = () => {
+  config.resources.forEach((resource) =>{
+    var r = {};
+    var collection = db.collection(resource.name);
+    if (resource.isMultiple !== undefined && resource.isMultiple === true){
+      r.GET = (req, res, id) => {
+          collection.findOne({_id:ObjectId(id)}).then((item) => {
+            if (item !== null){
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify(item));
+            }else{
+              res.writeHead(404, {'Content-Type':'application/json'});
+              res.end();
+            }
           });
-        });
       }
-    }
-    routing["/"+resource.name+"/:id"] = r;
+      if (resource.isWritable !== undefined && resource.isWritable === true)
+      {
+        r.DELETE = (req, res, id) => {
+          collection.deleteOne({_id:ObjectId(id)}).then((item) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end();
+          });
+        }
+        r.PUT = (req, res, id) => {
+          var jsonString = "";
+          req.on('data', data => jsonString += data);
+          req.on('end', () => {
+            var obj = JSON.parse(jsonString);
+            collection.updateOne({_id:ObjectId(id)}, obj, (err, item) => {
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify(obj));
+            });
+          });
+        }
+      }
+      routing["/"+resource.name+"/:id"] = r;
 
-    var all = {};
-    all.GET = (req, res, id) => {
+      var all = {};
+      all.GET = (req, res, id) => {
         collection.find({}).toArray((err, items) => {
           res.writeHead(200, {'Content-Type': 'application/json'});
           res.end(JSON.stringify(items));
         });
-    }
-    if (resource.isWritable !== undefined && resource.isWritable === true)
-    {
-      all.PUT = (req, res, id) => {
-        var jsonString = "";
-
-        req.on('data', data => jsonString += data);
-        req.on('end', () => {
-          var a = JSON.parse(jsonString);
-          collection.insertOne(a, (err, result)=>{
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(result.ops[0]));
-          });
-        });
       }
-    }
-    routing["/"+resource.name] = all;
-  }else{
-    r.GET = (req, res, next) => { //TODO
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(resource.name);
-    }
-    if (resource.isWritable !== undefined && resource.isWritable === true){
-      r.POST = (req, res, next) => { //TODO
+      if (resource.isWritable !== undefined && resource.isWritable === true)
+      {
+        all.PUT = (req, res, id) => {
+          var jsonString = "";
+          req.on('data', data => jsonString += data);
+          req.on('end', () => {
+            var obj = JSON.parse(jsonString);
+            collection.insertOne(obj, (err, result)=>{
+              res.writeHead(200, {'Content-Type': 'application/json'});
+              res.end(JSON.stringify(result.ops[0]));
+            });
+          });
+        }
+      }
+      routing["/"+resource.name] = all;
+    }else{
+      r.GET = (req, res, next) => { //TODO
           res.writeHead(200, {'Content-Type': 'application/json'});
           res.end(resource.name);
       }
+      if (resource.isWritable !== undefined && resource.isWritable === true){
+        r.POST = (req, res, next) => { //TODO
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(resource.name);
+        }
+      }
+      routing["/"+resource.name] = r;
     }
-    routing["/"+resource.name] = r;
-  }
-});
-
+  });
+};
 console.log(config);
 
 var url = "mongodb://127.0.0.1:27017/cms";
 MongoClient.connect(url, function(err, database) {
   console.log("Connected correctly to server");
   db = database;
-});
-
-var server = http.createServer(dispatch(routing));
-server.listen(PORT, function(){
-    console.log("Server started");
+  initHandlers();
+  var server = http.createServer(dispatch(routing));
+  server.listen(PORT, function(){
+      console.log("Server started");
+  });
 });
